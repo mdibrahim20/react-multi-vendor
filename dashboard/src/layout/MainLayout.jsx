1c00a4f4651e29241a1cf88edae9bff4271dc57c
Md Ibrahim Khalil <imuhammad784@gmail.com>
2025-07-20 22:51:02 +0600

    Dashboard Manage with Navigation along with the Header,sidebar design

--- dashboard/src/layout/MainLayout.jsx
+++ dashboard/src/layout/MainLayout.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+// MainLayout.js
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 const MainLayout = () => {
+    const [isDark, setIsDark] = useState(false);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    useEffect(() => {
+        if (isDark) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [isDark]);
+
     return (
-        <div>
-            <h1>Main Layout <Outlet/> </h1>
+        <div className={`flex h-screen font-sans p-5 gap-4 ${isDark ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-900'}`}>
+            <div className="rounded-2xl overflow-hidden">
+                <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+            </div>
+
+            <div className="flex flex-col flex-1 overflow-hidden rounded-2xl overflow-hidden">
+                <Header isDark={isDark} setIsDark={setIsDark} setSidebarOpen={setSidebarOpen} />
+
+                <main className="flex-1 overflow-y-auto p-6">
+                    <Outlet />
+                </main>
+            </div>
         </div>
     );
 };
